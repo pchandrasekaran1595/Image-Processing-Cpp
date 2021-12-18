82476c024d17321572f37138b398fba7d294598e
pchandrasekaran <jackstapleton920@gmail.com>
2021-12-18 19:57:19 +0530

    Update

--- Header.h
+++ Header.h
@@ -5,7 +5,9 @@
 #include <cstring>
 #include <Windows.h>
 #include <direct.h>
+#include <cmath>
 #include <opencv2/opencv.hpp>
+#include "opencv2/highgui.hpp"
 
 using namespace std;
 
@@ -47,9 +49,64 @@ public:
 class ImageProcessor {
 
 public:
-	cv::Mat proc_image;
 
-	void gauss_blur(cv::Mat image, int kernel_size) {
+	cv::Mat gauss_blur(cv::Mat image, int kernel_size) {
+		cv::Mat proc_image =  cv::Mat::zeros(image.size(), image.type());
 		cv::GaussianBlur(image, proc_image, cv::Size(kernel_size, kernel_size), 0);
+		return proc_image;
+	}
+
+	cv::Mat average_blur(cv::Mat image, int kernel_size) {
+		cv::Mat proc_image = cv::Mat::zeros(image.size(), image.type());
+		cv::blur(image, proc_image, cv::Size(kernel_size, kernel_size));
+		return proc_image;
+	}
+
+	cv::Mat median_blur(cv::Mat image, int kernel_size) {
+		cv::Mat proc_image = cv::Mat::zeros(image.size(), image.type());
+		cv::medianBlur(image, proc_image, kernel_size);
+		return proc_image;
+	}
+
+	cv::Mat gammaAdjust(cv::Mat image, double alpha) {
+		cv::Mat proc_image = cv::Mat::zeros(image.size(), image.type());
+		for (int y = 0; y < image.rows; y++) {
+			for (int x = 0; x < image.cols; x++) {
+				for (int c = 0; c < image.channels(); c++) {
+					proc_image.at<cv::Vec3b>(y, x)[c] = cv::saturate_cast<uchar>(image.at<cv::Vec3b>(y, x)[c] * alpha - 100);
+				}
+			}
+		}
+		return proc_image;
+	}
+
+	cv::Mat linearContrastAdjust(cv::Mat image, double alpha) {
+		cv::Mat proc_image = cv::Mat::zeros(image.size(), image.type());
+		for (int y = 0; y < image.rows; y++) {
+			for (int x = 0; x < image.cols; x++) {
+				for (int c = 0; c < image.channels(); c++) {
+					proc_image.at<cv::Vec3b>(y, x)[c] = cv::saturate_cast<uchar>(image.at<cv::Vec3b>(y, x)[c] + alpha);
+				}
+			}
+		}
+		return proc_image;
+	}
+
+	cv::Mat adaptiveEqualization(cv::Mat image, double clipLimit, int tileGridSize) {
+		cv::Mat proc_image = cv::Mat::zeros(image.size(), image.type());
+
+		std::vector<cv::Mat> proc_bgr_split(3);
+		cv::split(proc_image, proc_bgr_split);
+
+		std::vector<cv::Mat> bgr_split(3);
+		cv::split(image, bgr_split);
+
+		cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(clipLimit, cv::Size(tileGridSize, tileGridSize));
+		for (int c = 0; c < image.channels(); c++) {
+			clahe->apply(bgr_split[c], proc_bgr_split[c]);
+		}
+		cv::merge(proc_bgr_split, proc_image);
+
+		return proc_image;
 	}
 };
--- Image-Processing-Cpp.cpp
+++ Image-Processing-Cpp.cpp
@@ -10,21 +10,49 @@ wstring getPath() {
 
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[]){
 
 	const wstring path = getPath();
 	const char* args_1[2] = { "--file", "-f" };
 	const char* args_2[2] = { "--gauss-blur", "-gb" };
+	const char* args_3[2] = { "--average-blur", "-ab" };
+	const char* args_4[2] = { "--median-blur", "-mb" };
+	const char* args_5[2] = {"--gamma", "-g"};
+	const char* args_6[2] = { "--linear", "-l" };
+	const char* args_7[2] = { "--clahe", "-ae" };
+
 	const char* args_19[2] = { "--save", "-s" };
 
 	int args_1_len = sizeof(args_1) / sizeof(args_1[0]);
 	int args_2_len = sizeof(args_2) / sizeof(args_2[0]);
+	int args_3_len = sizeof(args_3) / sizeof(args_3[0]);
+	int args_4_len = sizeof(args_4) / sizeof(args_4[0]);
+	int args_5_len = sizeof(args_5) / sizeof(args_5[0]);
+	int args_6_len = sizeof(args_6) / sizeof(args_6[0]);
+	int args_7_len = sizeof(args_7) / sizeof(args_7[0]);
 	int args_19_len = sizeof(args_19) / sizeof(args_19[0]);
 
 	string filename = "";
 
 	bool do_gaussian_blur = false;
-	int gaussian_kernel_size = 1;
+	int gaussian_blur_kernel_size = 1;
+
+	bool do_average_blur = false;
+	int average_blur_kernel_size = 1;
+
+	bool do_median_blur = false;
+	int median_blur_kernel_size = 1;
+
+	bool do_gamma_adjust = false;
+	double gamma_alpha = 1.0;
+
+	bool do_linear_adjust = false;
+	double linear_alpha = 0.0;
+
+	bool do_clahe = false;
+	double clipLimit = 2.0;
+	int tileGridSize = 2;
+
 	bool save = false;
 
 	for (int i = 0; i < argc; i++) {
@@ -38,10 +66,55 @@ int main(int argc, char *argv[]){
 		for (int j = 0; j < args_2_len; j++) {
 			if (strcmp(args_2[j], argv[i]) == 0) {
 				do_gaussian_blur = true;
-				gaussian_kernel_size = stoi(argv[i + 1]);
-				if (gaussian_kernel_size % 2 == 0) {
-					gaussian_kernel_size++;
+				gaussian_blur_kernel_size = stoi(argv[i + 1]);
+				if (gaussian_blur_kernel_size % 2 == 0) {
+					gaussian_blur_kernel_size++;
 				}
+			}
+		}
+
+		for (int j = 0; j < args_3_len; j++) {
+			if (strcmp(args_3[j], argv[i]) == 0) {
+				do_average_blur = true;
+				average_blur_kernel_size = stoi(argv[i + 1]);
+			}
+		}
+
+		for (int j = 0; j < args_4_len; j++) {
+			if (strcmp(args_4[j], argv[i]) == 0) {
+				do_median_blur = true;
+				median_blur_kernel_size = stoi(argv[i + 1]);
+				if (median_blur_kernel_size % 2 == 0) {
+					median_blur_kernel_size++;
+				}
+			}
+		}
+
+		for (int j = 0; j < args_5_len; j++) {
+			if (strcmp(args_5[j], argv[i]) == 0) {
+				do_gamma_adjust = true;
+				gamma_alpha = stod(argv[i + 1]);
+			}
+		}
+
+		for (int j = 0; j < args_6_len; j++) {
+			if (strcmp(args_6[j], argv[i]) == 0) {
+				do_linear_adjust = true;
+				linear_alpha = stod(argv[i + 1]);
+				if (linear_alpha > 255) {
+					linear_alpha = 255.0;
+				}
+				if (linear_alpha < 0) {
+					linear_alpha = 0.0;
+				}
+			}
+		}
+
+		for (int j = 0; j < args_7_len; j++) {
+			if (strcmp(args_7[j], argv[i]) == 0) {
+				do_clahe = true;
+				clipLimit = stod(argv[i + 1]);
+				tileGridSize = stoi(argv[i + 2]);
 			}
 		}
 
@@ -58,9 +131,8 @@ int main(int argc, char *argv[]){
 	string SAVE_PATH = string(path.begin(), path.end()) + "\\Processed";
 
 	const char* save_path_array = SAVE_PATH.c_str();
-
 	if (_mkdir(save_path_array) == -1)
-		cout << "Cannot Create Directory !!!!";
+		cout << "Directory Already Present !!!!";
 
 
 	ImageHandler image_handler;
@@ -68,12 +140,17 @@ int main(int argc, char *argv[]){
 
 	cv::Mat image = image_handler.read_image(READ_PATH);
 	
-	if (do_gaussian_blur) image_processor.gauss_blur(image, gaussian_kernel_size);
+	if (do_gaussian_blur) image = image_processor.gauss_blur(image, gaussian_blur_kernel_size);
+	if (do_average_blur)  image = image_processor.average_blur(image, average_blur_kernel_size);
+	if (do_median_blur)   image = image_processor.median_blur(image, median_blur_kernel_size);
+	if (do_gamma_adjust)  image = image_processor.gammaAdjust(image, gamma_alpha);
+	if (do_linear_adjust) image = image_processor.linearContrastAdjust(image, linear_alpha);
+	if (do_clahe)         image = image_processor.adaptiveEqualization(image, clipLimit, tileGridSize);
 
 	if (!save)
-		image_handler.show_image(image_handler.downscale(image_processor.proc_image));
+		image_handler.show_image(image_handler.downscale(image));
 	else
-		image_handler.save_image(SAVE_PATH + "\\Processed.jpg", image_processor.proc_image);
+		image_handler.save_image(SAVE_PATH + "\\Processed.jpg", image);
 		
 
 	return 0;
